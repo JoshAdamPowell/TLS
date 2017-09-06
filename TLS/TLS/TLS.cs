a36fe59e773a17f6545b65977bf00dd1a1750b19
Josh Powell <Josh.Powell@softwire.com>
2017-09-06 10:25:02 +0100

    Will now find all TRA TLRs

--- TLS/TLS/TLS.cs
+++ TLS/TLS/TLS.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace TLS
 {
@@ -6,7 +8,22 @@ namespace TLS
     {
         static void Main(string[] args)
         {
-            
+            string input = System.IO.File.ReadAllText(@"c:\work\training\TLS\SampleText.txt");
+            int length = input.Length;
+            int counter = 0;
+            for (var i = 0; i < length; i++)
+            {
+               
+                if ((input[i] == 't') && (input[i+1] == 'r') && (input[i+2] == 'a'))
+                {
+                    counter = counter + 1;
+                }
+            }
+
+
+            Console.WriteLine(counter);
+            Console.ReadKey();
+
         }
     }
 }
