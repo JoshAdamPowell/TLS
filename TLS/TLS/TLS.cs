58bc3c6c98bdca998a94b2180849e5b063c20fbc
Josh Powell <Josh.Powell@softwire.com>
2017-09-06 10:54:05 +0100

    Now uses regex rather than naive method

--- TLS/TLS/TLS.cs
+++ TLS/TLS/TLS.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace TLS
 {
@@ -11,15 +12,10 @@ namespace TLS
             string input = System.IO.File.ReadAllText(@"c:\work\training\TLS\SampleText.txt");
             int length = input.Length;
             int counter = 0;
-            for (var i = 0; i < length; i++)
-            {
-               
-                if ((input[i] == 't'|| input[i] == 'T') && (input[i+1] == 'r'|| input[i + 1] == 'R') && (input[i+2] == 'a'|| input[i + 2] == 'A'))
-                {
-                    counter = counter + 1;
-                }
-            }
+            Regex tra = new Regex("tra", RegexOptions.IgnoreCase);
 
+            var matches = tra.Matches(input);
+            counter = matches.Count;
 
             Console.WriteLine(counter);
             Console.ReadKey();
